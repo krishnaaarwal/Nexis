b8587122c6b73df02a0fc29b6c886e7c412964a3
krishnaaarwal <agrkrishna123@gmail.com>
2026-04-06 19:18:41 +0530

    Fix bugs and Auth service registered as Eureka client

--- eureka-server/src/main/java/com/nexis/eureka_server/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/nexis/eureka_server/EurekaServerApplication.java
@@ -1,15 +1,20 @@
 package com.nexis.eureka_server;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class EurekaServerApplication {
+public class EurekaServerApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaServerApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+
+	}
 }
