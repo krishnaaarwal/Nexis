3487b6de159b886335aa995b58b8c783e44e52f2
krishnaaarwal <agrkrishna123@gmail.com>
2026-05-03 12:55:32 +0530

    Setup Execution service with dependency  and Version upgrade of all service from 3.5.13 -> 3.5.14

---
+++ execution-service/src/test/java/com/nexis/execution_service/ExecutionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nexis.execution_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExecutionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
