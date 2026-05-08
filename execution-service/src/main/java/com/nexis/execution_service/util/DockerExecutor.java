02bc3004fafddb54b7c7239d3b9731ba71cc1924
krishnaaarwal <agrkrishna123@gmail.com>
2026-05-08 22:14:31 +0530

    Docker client logic

--- execution-service/src/main/java/com/nexis/execution_service/util/DockerExecutor.java
+++ execution-service/src/main/java/com/nexis/execution_service/util/DockerExecutor.java
@@ -1,18 +1,21 @@
 package com.nexis.execution_service.util;
 
 import com.github.dockerjava.api.DockerClient;
-import com.github.dockerjava.api.command.CreateContainerCmd;
+import com.github.dockerjava.api.async.ResultCallback;
 import com.github.dockerjava.api.command.CreateContainerResponse;
+import com.github.dockerjava.api.model.Frame;
 import com.github.dockerjava.api.model.HostConfig;
-import com.github.dockerjava.core.command.CreateContainerCmdImpl;
+import com.github.dockerjava.api.model.StreamType;
 import com.nexis.execution_service.config.type.CodeLanguage;
+import com.nexis.execution_service.config.type.StatusType;
 import com.nexis.execution_service.entity.ExecutionResult;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import java.util.UUID;
+import java.util.concurrent.TimeUnit;
 
-    @Component
+@Component
     @Slf4j
     @RequiredArgsConstructor
     public class DockerExecutor {
@@ -20,6 +23,9 @@ import java.util.UUID;
         private final DockerClient dockerClient;
 
         public ExecutionResult execute(UUID jobId, CodeLanguage codeLanguage, String code) {
+
+            StringBuilder stdout = new StringBuilder();
+            StringBuilder stderr = new StringBuilder();
 
             ContainerSetup setup = getContainerSetup(codeLanguage, code);
 
@@ -32,7 +38,8 @@ import java.util.UUID;
                     .withMemory(134217728L) // 128 MB in bytes (128 * 1024 * 1024)
                     .withNetworkMode("none"); // Completely disables internet access inside the container
 
-            // 4. Build and send the Create Command to the Docker Daemon (a Builder pattern)
+
+        // Build and send the Create Command to the Docker Daemon (a Builder pattern)
             CreateContainerResponse container = dockerClient.createContainerCmd(setup.image())
                     .withName("nexis-job-" + jobId.toString())
                     .withCmd(setup.command())
@@ -42,7 +49,57 @@ import java.util.UUID;
             String containerId = container.getId();
             System.out.println("Created container: " + containerId);
 
-            return null;
+            // Step 2 - Start the container
+            dockerClient.startContainerCmd(containerId).exec();
+
+            // Step 3 - Read the output
+            try{
+                dockerClient.logContainerCmd(containerId)
+                        .withStdOut(true) //I want std output
+                        .withStdErr(true) //I want std error
+                        .withFollowStream(true) // Keep listening until the container stops
+                        .exec(new ResultCallback.Adapter<>(){
+
+                            // A Frame consists of two things:
+                            //
+                            //    The Header (Type): A tiny label that says, "Hey, this package is STDOUT" or "Hey, this package is STDERR".
+                            //
+                            //    The Payload: The actual byte array of the text (e.g., the word "Hello").
+
+
+                            /*onNext(Frame frame):
+                            The Workhorse. This fires every single time a piece of text comes out of the container.
+                            If the container prints 100 lines, this method fires 100 times. Inside here,
+                            you write the if/else logic to append the payload to your stdout or stderr StringBuilders.*/
+                            @Override
+                            public void onNext(Frame frame) {
+                                if(frame.getStreamType() == StreamType.STDOUT){
+                                    stdout.append(new String(frame.getPayload()));
+                                } else if (frame.getStreamType() == StreamType.STDERR) {
+                                    stderr.append(new String(frame.getPayload()));
+                                }
+                            }
+                        })
+                        .awaitCompletion(30, TimeUnit.SECONDS);  // It tells your Java thread:
+                                                                        // "Stop right here. Do not execute the next line of code until the onComplete method fires.
+                                                                        // ... OR until 30 seconds pass."
+
+                /*If a user writes a while(true) loop in their workspace, the container will run forever.
+                Without this timeout, your Java thread gets permanently blocked.
+                With the timeout, after 30 seconds, Java forcefully throws an InterruptedException,
+                 escapes the roadblock, and you can fail the job gracefully.*/
+            } catch (Exception e) {
+                stderr.append("\nExecution timed out after 30 seconds.");
+            }
+
+            // Step 4 - Destroy the container
+            dockerClient.removeContainerCmd(containerId).withForce(true).exec();
+
+            StatusType finalStatus = StatusType.COMPLETED;
+            if (stderr.length() > 0) {
+                finalStatus = StatusType.FAILED;
+            }
+            return new ExecutionResult(jobId,finalStatus,stdout.toString(),stderr.toString());
         }
 
         private record ContainerSetup(String image, String[] command) {}
